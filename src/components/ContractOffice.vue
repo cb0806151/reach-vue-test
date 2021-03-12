2aeb61a0d014078d641ebb6170ab802a60852c70
cb0806151 <cb0806151@otc.edu>
2021-03-12 10:53:11 -0600

    style elements + change entry point

--- src/components/ContractOffice.vue
+++ src/components/ContractOffice.vue
@@ -1,16 +1,18 @@
 <template>
-  <div>
+  <div id="container">
     <button>Connect Wallet</button>
     <button>Add 1 token to wallet</button>
-    <div>
-      <button>Deploy contract</button>
-      <textarea><textarea>
-      <button>Copy Invite<button>
-    </div>
-    <div>
-      <button>Join Contract</button>
-      <textarea><textarea>
-      <button>Submit Invite</button>
+    <div id="pane-container">
+      <div class="pane">
+        <button>Deploy contract</button>
+        <textarea></textarea>
+        <button>Copy Invite</button>
+      </div>
+      <div class="pane">
+        <button>Join Contract</button>
+        <textarea></textarea>
+        <button>Submit Invite</button>
+      </div>
     </div>
   </div>
 </template>
@@ -24,3 +26,29 @@ export default {
   }
 }
 </script>
+
+<style>
+button {
+  margin: 10px;
+}
+#container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+#pane-container {
+  display: flex;
+  flex-direction: row;
+}
+
+.pane {
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  border: 2px solid black;
+  margin: 10px;
+}
+
+</style>
