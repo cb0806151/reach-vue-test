7241531b7bb549735ecdf8491c902e7761435b09
cb0806151 <cb0806151@otc.edu>
2021-03-12 10:39:29 -0600

    create basic html for application

---
+++ src/components/ContractOffice.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <button>Connect Wallet</button>
+    <button>Add 1 token to wallet</button>
+    <div>
+      <button>Deploy contract</button>
+      <textarea><textarea>
+      <button>Copy Invite<button>
+    </div>
+    <div>
+      <button>Join Contract</button>
+      <textarea><textarea>
+      <button>Submit Invite</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ContractOffice',
+  props: {
+    msg: String,
+    account: Object,
+  }
+}
+</script>
